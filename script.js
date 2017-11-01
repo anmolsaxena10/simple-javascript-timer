aa914a68f7df1a52d8966c8ff974ce8387e48f3b
Anmol Saxena <anmol.saxena10@gmail.com>
2017-11-01 20:00:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var finalDate;
+var millsPsec = 1000;
+var millsPmin = millsPsec*60;
+var millsPhr = millsPmin*60
+var millsPday = millsPhr*24;
+var t;
+
+$(document).ready(function(){
+	
+	$("#startCount").click(function(){
+		finalDate = new Date($("#targetDate").val());
+		t = setInterval(updateCounter,1000);
+	});
+});
+
+function updateCounter(){
+	
+	var currentDate = new Date();	
+	var remainingMills = finalDate.getTime() - currentDate.getTime();
+	
+	if(remainingMills > 0){
+	
+		var days = parseInt(remainingMills/millsPday);
+		var hrs = parseInt((remainingMills - days*millsPday)/millsPhr);
+		var mins = parseInt((remainingMills - days*millsPday - hrs*millsPhr)/millsPmin);
+		var secs = parseInt((remainingMills - days*millsPday - hrs*millsPhr - mins*millsPmin)/millsPsec);
+		
+		console.log(remainingMills);
+		
+		$("#displayCount").html("<h1>"+days+" Days "+hrs+"h:"+mins+"m:"+secs+"s</h1>");
+	}
+	else{
+		clearInterval(t);
+	}
+}
